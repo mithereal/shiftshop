e97eacd2aff35d0dbf6a93cea2112a331755ce0e
mithereal <mithereal@gmail.com>
2023-05-03 00:03:00 -0700

    initial commit

--- mix.exs
+++ mix.exs
@@ -49,7 +49,9 @@ defmodule Api.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:ueberauth_shift4shop, git: "https://github.com/mithereal/ueberauth_shift4shop.git"},
+      {:swift4shop, git: "https://github.com/mithereal/ex_swift4shop.git"}
     ]
   end
 
