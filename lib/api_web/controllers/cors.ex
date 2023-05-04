0b886d380189b179973d567ed62d88216bde0dcf
mithereal <mithereal@gmail.com>
2023-05-03 17:10:41 -0700

    .

--- lib/api_web/controllers/cors.ex
+++ lib/api_web/controllers/cors.ex
@@ -1,11 +1,11 @@
 defmodule ApiWeb.CORS do
-  endpoint = Application.compile_env(:api, ApiWeb.Endpoint)
+  endpoint = Application.get_env(:api, ApiWeb.Endpoint)
   origins = Keyword.get(endpoint, :origins) || "*"
 
   use Corsica.Router,
     origins: origins,
-    allow_credentials: Application.compile_env(:api, :allow_credentials, true),
-    max_age: Application.compile_env(:api, :max_age, 600)
+    allow_credentials: Application.get_env(:api, :allow_credentials, true),
+    max_age: Application.get_env(:api, :max_age, 600)
 
   resource("/*")
 
