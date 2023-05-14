fd9bac89814a48995095bebd8d38933806418ff5
mithereal <mithereal@gmail.com>
2023-05-14 14:21:36 -0700

    add login routes

--- lib/api_web/router.ex
+++ lib/api_web/router.ex
@@ -37,8 +37,6 @@ defmodule ApiWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
   end
 
   pipeline :browser_with_no_csrf do
